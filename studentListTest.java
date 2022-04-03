527a4df1a27bbd6de9def1a5551f5aa55c88f2b6
Hunk <s3756448@rmit.edu.vn>
2022-04-04 01:13:19 +0700

    Student List Test class

---
+++ studentListTest.java
@@ -0,0 +1,21 @@
+// import org.junit.Test;
+
+// import static org.junit.Assert.*;
+
+// public class studentListTest {
+
+//     @Test
+//     public void getStudent() {
+//         studentList.getStudent();
+//     }
+
+//     @Test
+//     public void readAllStudent() {
+//         studentList.readAllStudent();
+//     }
+
+//     @Test
+//     public void readStudentFromCSV() {
+//         studentList.readStudentFromCSV();
+//     }
+// }
