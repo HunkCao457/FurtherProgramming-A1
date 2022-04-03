7a14cc1b0f8a3da4395af8909afaf4bd22a332ba
Hunk <s3756448@rmit.edu.vn>
2022-04-04 00:40:07 +0700

    Student class

---
+++ Student.java
@@ -0,0 +1,42 @@
+public class Student{
+    private String id;
+    private String name;
+    private String birthDate;
+    
+    
+    public Student(String id, String name, String birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString(){
+        return "id=" + id + "," + " name=" + name + "," + " birthdate=" + birthDate;
+
+    }
+}
