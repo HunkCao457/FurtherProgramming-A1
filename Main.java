2da0bea84a59c2484c0e3d6754877c61acf51d25
Hunk <s3756448@rmit.edu.vn>
2022-04-04 01:17:58 +0700

    fixing

--- Main.java
+++ Main.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        // Scanner command = new Scanner(System.in);
-        // String data = command.nextLine();
-        // studentList.readAllStudent(data);
-        // studentList.getStudent();
-
+        
         startMenu.menu();
     }
 
