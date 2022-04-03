8665576a35b5019e7e511daed8114ee2f53d3da9
Hunk <s3756448@rmit.edu.vn>
2022-04-04 01:09:38 +0700

    Course List Test class

---
+++ courseListTest.java
@@ -0,0 +1,21 @@
+// import org.junit.Test;
+
+// import static org.junit.Assert.*;
+
+// public class courseListTest {
+
+//     @Test
+//     public void getCourse() {
+//         courseList.getCourse();
+//     }
+
+//     @Test
+//     public void readAllCourse() {
+//         courseList.readAllCourse();
+//     }
+
+//     @Test
+//     public void readCourseFromCSV() {
+//         courseList.readCourseFromCSV();
+//     }
+// }
