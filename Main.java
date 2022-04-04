c4cfaebea2d6a5f7307cf864428d2324a4c061bd
Hunk <s3756448@rmit.edu.vn>
2022-04-04 15:49:01 +0700

    commit

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
+/**
+ * The student enrollment management and data reading application that 
+ * will read all data file and make enrollment list depends on the users' input
+ * 
+ * @author  Hung Cao
+ * @version 1.0
+ * @since   2022-03-31
+ */
 
 public class Main{
     // Main driver method
